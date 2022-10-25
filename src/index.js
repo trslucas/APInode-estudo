0dddfdef603b1c8a7977d32e3e9ac18c30ea4bc0
Lucas Trindade <trslucas@outlook.com>
2022-10-25 16:13:25 -0300

    feat: Remover Conta

--- src/index.js
+++ src/index.js
@@ -135,4 +135,12 @@ app.put("/account", verifyIfExistsAccountCPF, (req, res) => {
     return res.status(201).send();
 });
 
+app.delete("/account", verifyIfExistsAccountCPF, (request, res) => {
+    const { customer } = request;
+
+    customers.splice(customer, 1);
+
+    return res.status(200).json(customers);
+});
+
 app.listen(3333);
