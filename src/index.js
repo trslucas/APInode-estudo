86b2c37997f139ba366591d56cc6f7f944c735c2
Lucas Trindade <trslucas@outlook.com>
2022-10-25 11:37:13 -0300

    feat: Cadastrando usuário

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
+const { response } = require("express");
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+app.use(express.json());
+
+const customers = [];
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement []
+ */
+
+app.post("/account", (req, res) => {
+    const { cpf, name } = req.body;
+    const id = uuidv4();
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: [],
+    });
+
+    return res.status(201).send();
+});
 
 app.listen(3333);
