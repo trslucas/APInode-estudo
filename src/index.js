d0d22bda56d0696f4899a75ade4ed48aadbc6177
Lucas Trindade <trslucas@outlook.com>
2022-10-25 13:11:05 -0300

    feat: Criando saque na conta

--- src/index.js
+++ src/index.js
@@ -55,4 +55,20 @@ app.get("/statement/", verifyIfExistsAccountCPF, (req, res) => {
     return res.json(customer.statement);
 });
 
+app.post("/deposit", verifyIfExistsAccountCPF, (req, res) => {
+    const { customer } = request;
+    const { description, amount } = req.body;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit",
+    };
+
+    customer.statement.push(statementOperation);
+
+    return res.status(201).send();
+});
+
 app.listen(3333);
